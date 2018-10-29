8f5cd60fc0fae5bb4ddfc241a30c52dc928e52af
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2018-10-29 15:11:32 +0530

    almost there

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const searchButton = document.getElementsByClassName("search"),
 	journeySummary = document.getElementById("journey"),
 	scheduleSummary = document.getElementById("schedule")
 
-let filteredData = [],
+let depFlightData = [],
+	retFlightData = [],
 	resultsTemplate = ``
 
 Array.from(searchButton).forEach(button =>
@@ -29,23 +30,36 @@ function filterResults(data) {
 			document.querySelector(".tab-pane.active #ret-date").value,
 		passangers = document.querySelector(".tab-pane.active #passangers").value,
 		minPrice = Math.trunc(priceSlider.noUiSlider.get()[0]),
-		maxPrice = Math.trunc(priceSlider.noUiSlider.get()[1])
+		maxPrice = Math.trunc(priceSlider.noUiSlider.get()[1]),
+		metaData = [origin, destination, depDate, retDate]
 
 	resultsTemplate = ``
 
-	const metaData = [origin, destination, depDate, retDate]
-	filteredData = data.filter(
+	depFlightData = data.filter(
 		flight =>
 			flight.origin.toLowerCase() === origin &&
 			flight.destination.toLowerCase() === destination &&
-			minPrice <= Number(flight.price) &&
-			Number(flight.price) <= maxPrice
+			minPrice <= flight.price &&
+			flight.price <= maxPrice &&
+			flight.date === depDate
 	)
 
-	renderResults(filteredData, metaData)
+	if (retDate) {
+		retFlightData = data.filter(
+			flight =>
+				flight.origin.toLowerCase() === destination &&
+				flight.destination.toLowerCase() === origin &&
+				minPrice <= flight.price &&
+				flight.price <= maxPrice &&
+				flight.date === depDate &&
+				flight.date === retDate
+		)
+	}
+
+	renderResults(depFlightData, retFlightData, metaData)
 }
 
-function renderResults(filteredData, metaData) {
+function renderResults(depFlightData, retFlightData, metaData) {
 	journeySummary.innerHTML = `${metaData[0]} > ${metaData[1]} 
   ${metaData[3] ? " > " + metaData[0] : ""}`
 
@@ -53,22 +67,27 @@ function renderResults(filteredData, metaData) {
 		metaData[3] === null ? "-" : metaData[3]
 	}`
 
-	filteredData.forEach(flight => {
+	depFlightData.forEach((flight, index) => {
 		resultsTemplate += `
       <div class="col-md-9">
-      <h3>total price</h3>
+      <h3>Rs. ${Math.ceil(depFlightData[index].price)}</h3>
         <div class="row">
           <div class="col-md-6">
-            <p>flight num</p>
-            <p>to</p>
-            <p>depart</p>
-            <p>arrive</p>
+            <p>${depFlightData[index].id}</p>
+            <p>${depFlightData[index].origin} > ${
+			depFlightData[index].destination
+		}</p>
+            <p>Depart: ${depFlightData[index].dep_time}</p>
+            <p>Arrive: ${depFlightData[index].arr_time}</p>
           </div>
           <div class="col-md-6">
-            <p>flight num</p>
-            <p>return</p>
-            <p>depart</p>
-            <p>arrive</p>
+          <p>${depFlightData[index].id}</p>
+          <p>${retFlightData[index].origin} > ${
+			retFlightData[index].destination
+		} 
+  </p>
+          <p>Depart: ${retFlightData[index].dep_time}</p>
+          <p>Arrive: ${retFlightData[index].arr_time}</p>
           </div>
         </div>
       </div>
